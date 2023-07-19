6139f389685c46609066c6ce2cfcbc8e0ba29846
jcrecio <juan.carlos.recio.abad@gmail.com>
2023-07-20 00:25:14 +0200

    Notebook for metrics. Remove legacy. Refactor.

--- open_api_model.py
+++ open_api_model.py
@@ -3,7 +3,7 @@ import time
 import os
 from objects import Error
 
-sleep_api = 0 # seconds
+sleep_api = 0
 
 predict_extraction_versions = {
     "v1": lambda x, y, z: """Given the initial java code: '{original_code}', and the following code: '{extraction_code}' which functionally is the same as the initial code, but having applied an extract refactor operation. Can you provide an appropriate meaningful method name for the extracted method '{method_to_compute}'? (Please provide the method name without parentheses)"""
@@ -27,38 +27,42 @@ predict_original_versions = {
     "v1": lambda x: """Given the following java code, suggest a name for the method without taking into account the current method name and provide only the method name in the answer without any additional data, symbols or spaces: """ + x,
 }
 
-def model_call(prompt):
-    try:
-        response = requests.post("https://api.openai.com/v1/chat/completions",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": os.getenv('OPEN_AI_KEY')
-            },
-            json={ "model": "gpt-4", 
-                  "prompt": prompt, 
-                  "n": 1, 
-                  "temperature": 0
-                }
-        )
-        if (response.status_code == 400):
-           return Error(response.content)
-        result = response.json()['choices'][0]['text'].strip()
-        return result
-    except:
-        return 'error'
+class OpenAiModel():
+    def __init__(self, model):
+        self.model = model
 
-def predict_extraction(original_code, extraction_code, method_to_compute, version = "v4"):
-    prediction = model_call(predict_extraction_versions[version](original_code, extraction_code, method_to_compute))
-    time.sleep(sleep_api)
-    if isinstance(prediction, Error): return prediction
-    return prediction.replace('()', '')
+    def chat(self, prompt):
+        try:
+            response = requests.post("https://api.openai.com/v1/chat/completions",
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": os.getenv('OPEN_AI_KEY')
+                },
+                json={ "model": self.model, 
+                       "prompt": prompt, 
+                       "n": 1, 
+                       "temperature": 0
+                    }
+            )
+            if (response.status_code == 400):
+                return Error(response.content)
+            result = response.json()['choices'][0]['text'].strip()
+            return result
+        except:
+            return 'error'
 
-def predict(input_code, method_name=None):
-    if (method_name == None):
-        prediction = model_call("""Given the following java code, suggest a name for the method without taking into account the current method name and provide only the method name in the answer without any additional data, symbols or spaces. (Do not provide more than one method name, and always use java standard and notation). This is the code: """ + input_code)
+    def predict_extraction(self, original_code, extraction_code, method_to_compute, version = "v4"):
+        prediction = self.chat(predict_extraction_versions[version](original_code, extraction_code, method_to_compute))
+        time.sleep(sleep_api)
+        if isinstance(prediction, Error): return prediction
+        return prediction.replace('()', '')
+
+    def predict(self, input_code, method_name=None):
+        if (method_name == None):
+            prediction = self.chat("""Given the following java code, suggest a name for the method without taking into account the current method name and provide only the method name in the answer without any additional data, symbols or spaces. (Do not provide more than one method name, and always use java standard and notation). This is the code: """ + input_code)
+            time.sleep(sleep_api)
+            return prediction.replace('()', '')
+        prediction = self.chat("""Given the following java code, suggest a name for the method : """ + method_name + ' without taking into account its current method name and provide only the method name in the answer without any additional data, symbols or spaces. (Do not provide more than one method name). This is the code: ' 
+                    + ' code: ' + input_code)
         time.sleep(sleep_api)
         return prediction.replace('()', '')
-    prediction = model_call("""Given the following java code, suggest a name for the method : """ + method_name + ' without taking into account its current method name and provide only the method name in the answer without any additional data, symbols or spaces. (Do not provide more than one method name). This is the code: ' 
-                  + ' code: ' + input_code)
-    time.sleep(sleep_api)
-    return prediction.replace('()', '')
--- run_experiment.py
+++ run_experiment.py
@@ -2,42 +2,69 @@ import sys
 import json
 import os
 
-from open_api_model import predict, predict_extraction
-from objects import ClassObject, ExtractionMethodContent, Method, OriginalMethodContent, Project, Error, serialize
-from utils import compute_method_similarity, get_dataset_item, log, read_file, write_file
+from open_api_model import OpenAiModel
+
+from objects import (
+    ClassObject,
+    ExtractionMethodContent,
+    Method,
+    OriginalMethodContent,
+    Project,
+    Error,
+    serialize,
+)
+from utils import (
+    compute_method_similarity,
+    get_dataset_item,
+    log,
+    read_file,
+    write_file,
+)
 
 from_folder = sys.argv[1]
 projects = dict()
+model = OpenAiModel(sys.argv[2])
 
 ########## Load dataset from folder argument ##########
 
-log('Loading dataset from {from_folder}...\n'.format(from_folder = from_folder))
+log("Loading dataset from {from_folder}...\n".format(from_folder=from_folder))
 for file_name in os.listdir(from_folder):
     code = read_file(os.path.join(from_folder, file_name))
     item = get_dataset_item(file_name, code)
 
-    if (item.project not in projects): 
+    if item.project not in projects:
         projects[item.project] = Project()
-    if (item.class_name not in projects[item.project].classes): 
+    if item.class_name not in projects[item.project].classes:
         projects[item.project].classes[item.class_name] = ClassObject()
-    if (item.method_name not in projects[item.project].classes[item.class_name].methods):
-        projects[item.project].classes[item.class_name] \
-            .methods[item.method_name] = Method()
-    if (item.variation == 'Original'): 
-        projects[item.project].classes[item.class_name] \
-            .methods[item.method_name].set_original(OriginalMethodContent(item.method_name, item.code.replace('\n', ''), file_name))
+    if item.method_name not in projects[item.project].classes[item.class_name].methods:
+        projects[item.project].classes[item.class_name].methods[
+            item.method_name
+        ] = Method()
+    if item.variation == "Original":
+        projects[item.project].classes[item.class_name].methods[
+            item.method_name
+        ].set_original(
+            OriginalMethodContent(
+                item.method_name, item.code.replace("\n", ""), file_name
+            )
+        )
     else:
-        projects[item.project].classes[item.class_name] \
-            .methods[item.method_name].add_extraction(ExtractionMethodContent(item.method_name, item.code.replace('\n', ''), file_name, item.variation))
-log('Dataset fully loaded.\n')
+        projects[item.project].classes[item.class_name].methods[
+            item.method_name
+        ].add_extraction(
+            ExtractionMethodContent(
+                item.method_name, item.code.replace("\n", ""), file_name, item.variation
+            )
+        )
+log("Dataset fully loaded.\n")
 
 
 ########## Run the experiment to get the method names prediction ##########
 
-log('Starting methods prediction...')
+log("Starting methods prediction...")
 
 for project_key, project in projects.items():
-    print('\n\n')
+    print("\n\n")
     log("- Project: " + project_key)
 
     for class_key, class_name in project.classes.items():
@@ -45,7 +72,9 @@ for project_key, project in projects.items():
 
         for method_key, method in class_name.methods.items():
             log("----- Target method: " + method.original.method_name)
-            if (os.path.exists('output/' + project_key + '.' + class_key + '.' + method_key + '.json')):
+            if os.path.exists(
+                "output/" + project_key + "." + class_key + "." + method_key + ".json"
+            ):
                 log("------ Method prediction already exists, skipping method...")
                 continue
 
@@ -54,20 +83,33 @@ for project_key, project in projects.items():
 
             halt_method = False
             for extraction in method.extractions:
-                if (halt_method == True):
+                if halt_method == True:
                     break
                 extraction_with_previous_predictions = extraction.code
 
                 for acc in accumulative_extractions_predictions:
-                    extraction_with_previous_predictions \
-                        = extraction_with_previous_predictions \
-                            .replace(acc, accumulative_extractions_predictions[acc]) \
-                            .replace(acc + "_method", accumulative_extractions_predictions[acc])
+                    extraction_with_previous_predictions = (
+                        extraction_with_previous_predictions.replace(
+                            acc, accumulative_extractions_predictions[acc]
+                        ).replace(
+                            acc + "_method", accumulative_extractions_predictions[acc]
+                        )
+                    )
 
-                next_extraction_index = extraction_with_previous_predictions.find(method_key + '_extraction_')
-                next_extraction = extraction_with_previous_predictions[next_extraction_index: next_extraction_index + 1 + len(method_key + "_extraction_")]
+                next_extraction_index = extraction_with_previous_predictions.find(
+                    method_key + "_extraction_"
+                )
+                next_extraction = extraction_with_previous_predictions[
+                    next_extraction_index : next_extraction_index
+                    + 1
+                    + len(method_key + "_extraction_")
+                ]
 
-                extraction_prediction_response = predict_extraction(method.original.code, extraction_with_previous_predictions, next_extraction)
+                extraction_prediction_response = model.predict_extraction(
+                    method.original.code,
+                    extraction_with_previous_predictions,
+                    next_extraction,
+                )
 
                 if isinstance(extraction_prediction_response, Error):
                     extraction_prediction_response.method_name = method_key
@@ -75,25 +117,43 @@ for project_key, project in projects.items():
                     break
 
                 extraction.prediction = extraction_prediction_response
-                extraction.updated_code = extraction_with_previous_predictions \
-                    .replace(next_extraction, extraction.prediction) \
-                    .replace(next_extraction + "_method", extraction.prediction)
+                extraction.updated_code = extraction_with_previous_predictions.replace(
+                    next_extraction, extraction.prediction
+                ).replace(next_extraction + "_method", extraction.prediction)
 
-                accumulative_extractions_predictions[next_extraction] = extraction.prediction
-                
+                accumulative_extractions_predictions[
+                    next_extraction
+                ] = extraction.prediction
+
                 log("------- Extraction: " + extraction.extraction_name)
                 log("------- Prediction: " + extraction.prediction)
 
-                original_prediction_after_next_extraction = predict(extraction.updated_code)
-                method.original.predictions.append(original_prediction_after_next_extraction)
-                similarity = compute_method_similarity(method.original.method_name, original_prediction_after_next_extraction)
+                original_prediction_after_next_extraction = model.predict(
+                    extraction.updated_code
+                )
+                method.original.predictions.append(
+                    original_prediction_after_next_extraction
+                )
+                similarity = compute_method_similarity(
+                    method.original.method_name,
+                    original_prediction_after_next_extraction,
+                )
                 method.original.similarities.append(similarity)
-                log("----- Predicted method for target method after extraction {extraction_number}: {prediction}"
-                    .format(extraction_number=extraction_number, prediction= original_prediction_after_next_extraction))
-                log("----- Similarity between original method name and predicted method name after extraction {extraction_number}: {similarity} "
-                    .format(extraction_number=extraction_number, similarity=str(similarity)))
+                log(
+                    "----- Predicted method for target method after extraction {extraction_number}: {prediction}".format(
+                        extraction_number=extraction_number,
+                        prediction=original_prediction_after_next_extraction,
+                    )
+                )
+                log(
+                    "----- Similarity between original method name and predicted method name after extraction {extraction_number}: {similarity} ".format(
+                        extraction_number=extraction_number, similarity=str(similarity)
+                    )
+                )
                 extraction_number += 1
 
             json_str = json.dumps(method, default=serialize, indent=4)
-            write_file('output/' + project_key + '.' + class_key + '.' + method_key + '.json', json_str)
-
+            write_file(
+                "output/" + project_key + "." + class_key + "." + method_key + ".json",
+                json_str,
+            )
